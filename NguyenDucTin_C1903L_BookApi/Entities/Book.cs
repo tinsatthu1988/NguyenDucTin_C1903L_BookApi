41a52c2a26dde4991b58b7311038f35b8265848d
TIN-THIET-KE <TIN-THIET-KE@TIN-THIET-KE>
2021-03-26 08:42:26 +0700

    Add project files.

---
+++ NguyenDucTin_C1903L_BookApi/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NguyenDucTin_C1903L_BookApi.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public DateTime DatePublished { get; set; } = DateTime.Now;
+        public string PhotoUrl { get; set; }
+    }
+}
---
+++ NguyenDucTin_C1903L_BookApi/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace NguyenDucTin_C1903L_BookApi
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
